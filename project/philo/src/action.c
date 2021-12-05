09b9286aeb8a6d87609cea76c5c371282b700ad6
hashlyschool <ilya12-141998@mail.ru>
2021-12-05 19:43:02 +0300

    Mandatory. Norma - OK, checklist - OK, valgrind - OK

--- project/philo/src/action.c
+++ project/philo/src/action.c
@@ -6,7 +6,7 @@
 /*   By: hashly <hashly@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 10:20:44 by hashly            #+#    #+#             */
-/*   Updated: 2021/12/05 19:00:13 by hashly           ###   ########.fr       */
+/*   Updated: 2021/12/05 19:40:13 by hashly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_take_forks(t_philo *philo)
 	if (philo->data->death == 1)
 		return (0);
 	pthread_mutex_lock(&philo->data->forks[philo->min_fork]);
-	if (ft_write_status(philo, TAKE_FORK) == 0)
+	if (ft_write_status(philo, TAKE_FORK) == 0 || philo->data->num_phil == 1)
 	{
 		pthread_mutex_unlock(&philo->data->forks[philo->min_fork]);
 		return (0);
@@ -42,7 +42,6 @@ int	ft_eat(t_philo *philo)
 		pthread_mutex_unlock(&philo->data->forks[philo->max_fork]);
 		return (0);
 	}
-	philo->last_eat = get_time_ms();
 	ft_usleep(philo->data, philo->data->t_eat);
 	philo->last_eat = get_time_ms();
 	pthread_mutex_unlock(&philo->data->forks[philo->max_fork]);
